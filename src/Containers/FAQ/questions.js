3aab74d93d9aa8f21e3e663c01181cec8b475b36
agasheadwait <agasheadwait24@gmail.com>
2020-12-13 13:04:30 +0530

    Added FAQ functionality

--- src/Containers/FAQ/questions.js
+++ src/Containers/FAQ/questions.js
@@ -1,8 +1,54 @@
 const questions = [
-  { question: "LOrem", answer: "Ipsum" },
-  { question: "LOrem", answer: "Ipsum" },
-  { question: "LOrem", answer: "Ipsum" },
-  { question: "LOrem", answer: "Ipsum" },
+  {
+    question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget odio est. Pellentesque elementum, diam sit amet facilisis gravida, nisi augue rhoncus nibh, quis sodales lorem nisl at ligula. Morbi accumsan iaculis mauris quis semper. Ut sodales justo eu elit pellentesque hendrerit. Aliquam ut ipsum neque. Nulla tincidunt egestas ante quis vestibulum. Suspendisse rhoncus egestas urna, id vestibulum nunc porttitor ac. Pellentesque sit amet dui magna. Sed porta, augue ac bibendum pharetra, nunc ipsum dictum felis, vel tristique justo tortor eu diam. Cras eu risus risus. Fusce iaculis finibus gravida. Sed eu diam pharetra, condimentum orci quis, mollis magna.",
+  },
+  {
+    question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.2",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget odio est. Pellentesque elementum, diam sit amet facilisis gravida, nisi augue rhoncus nibh, quis sodales lorem nisl at ligula. Morbi accumsan iaculis mauris quis semper. Ut sodales justo eu elit pellentesque hendrerit. Aliquam ut ipsum neque. Nulla tincidunt egestas ante quis vestibulum. Suspendisse rhoncus egestas urna, id vestibulum nunc porttitor ac. Pellentesque sit amet dui magna. Sed porta, augue ac bibendum pharetra, nunc ipsum dictum felis, vel tristique justo tortor eu diam. Cras eu risus risus. Fusce iaculis finibus gravida. Sed eu diam pharetra, condimentum orci quis, mollis magna.",
+  },
+  {
+    question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.3",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget odio est. Pellentesque elementum, diam sit amet facilisis gravida, nisi augue rhoncus nibh, quis sodales lorem nisl at ligula. Morbi accumsan iaculis mauris quis semper. Ut sodales justo eu elit pellentesque hendrerit. Aliquam ut ipsum neque. Nulla tincidunt egestas ante quis vestibulum. Suspendisse rhoncus egestas urna, id vestibulum nunc porttitor ac. Pellentesque sit amet dui magna. Sed porta, augue ac bibendum pharetra, nunc ipsum dictum felis, vel tristique justo tortor eu diam. Cras eu risus risus. Fusce iaculis finibus gravida. Sed eu diam pharetra, condimentum orci quis, mollis magna.",
+  },
+  {
+    question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.4",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget odio est. Pellentesque elementum, diam sit amet facilisis gravida, nisi augue rhoncus nibh, quis sodales lorem nisl at ligula. Morbi accumsan iaculis mauris quis semper. Ut sodales justo eu elit pellentesque hendrerit. Aliquam ut ipsum neque. Nulla tincidunt egestas ante quis vestibulum. Suspendisse rhoncus egestas urna, id vestibulum nunc porttitor ac. Pellentesque sit amet dui magna. Sed porta, augue ac bibendum pharetra, nunc ipsum dictum felis, vel tristique justo tortor eu diam. Cras eu risus risus. Fusce iaculis finibus gravida. Sed eu diam pharetra, condimentum orci quis, mollis magna.",
+  },
+  {
+    question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.5",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget odio est. Pellentesque elementum, diam sit amet facilisis gravida, nisi augue rhoncus nibh, quis sodales lorem nisl at ligula. Morbi accumsan iaculis mauris quis semper. Ut sodales justo eu elit pellentesque hendrerit. Aliquam ut ipsum neque. Nulla tincidunt egestas ante quis vestibulum. Suspendisse rhoncus egestas urna, id vestibulum nunc porttitor ac. Pellentesque sit amet dui magna. Sed porta, augue ac bibendum pharetra, nunc ipsum dictum felis, vel tristique justo tortor eu diam. Cras eu risus risus. Fusce iaculis finibus gravida. Sed eu diam pharetra, condimentum orci quis, mollis magna.",
+  },
+  {
+    question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.6",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget odio est. Pellentesque elementum, diam sit amet facilisis gravida, nisi augue rhoncus nibh, quis sodales lorem nisl at ligula. Morbi accumsan iaculis mauris quis semper. Ut sodales justo eu elit pellentesque hendrerit. Aliquam ut ipsum neque. Nulla tincidunt egestas ante quis vestibulum. Suspendisse rhoncus egestas urna, id vestibulum nunc porttitor ac. Pellentesque sit amet dui magna. Sed porta, augue ac bibendum pharetra, nunc ipsum dictum felis, vel tristique justo tortor eu diam. Cras eu risus risus. Fusce iaculis finibus gravida. Sed eu diam pharetra, condimentum orci quis, mollis magna.",
+  },
+  {
+    question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.7",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget odio est. Pellentesque elementum, diam sit amet facilisis gravida, nisi augue rhoncus nibh, quis sodales lorem nisl at ligula. Morbi accumsan iaculis mauris quis semper. Ut sodales justo eu elit pellentesque hendrerit. Aliquam ut ipsum neque. Nulla tincidunt egestas ante quis vestibulum. Suspendisse rhoncus egestas urna, id vestibulum nunc porttitor ac. Pellentesque sit amet dui magna. Sed porta, augue ac bibendum pharetra, nunc ipsum dictum felis, vel tristique justo tortor eu diam. Cras eu risus risus. Fusce iaculis finibus gravida. Sed eu diam pharetra, condimentum orci quis, mollis magna.",
+  },
+  {
+    question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.8",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget odio est. Pellentesque elementum, diam sit amet facilisis gravida, nisi augue rhoncus nibh, quis sodales lorem nisl at ligula. Morbi accumsan iaculis mauris quis semper. Ut sodales justo eu elit pellentesque hendrerit. Aliquam ut ipsum neque. Nulla tincidunt egestas ante quis vestibulum. Suspendisse rhoncus egestas urna, id vestibulum nunc porttitor ac. Pellentesque sit amet dui magna. Sed porta, augue ac bibendum pharetra, nunc ipsum dictum felis, vel tristique justo tortor eu diam. Cras eu risus risus. Fusce iaculis finibus gravida. Sed eu diam pharetra, condimentum orci quis, mollis magna.",
+  },
+  {
+    question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.9",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget odio est. Pellentesque elementum, diam sit amet facilisis gravida, nisi augue rhoncus nibh, quis sodales lorem nisl at ligula. Morbi accumsan iaculis mauris quis semper. Ut sodales justo eu elit pellentesque hendrerit. Aliquam ut ipsum neque. Nulla tincidunt egestas ante quis vestibulum. Suspendisse rhoncus egestas urna, id vestibulum nunc porttitor ac. Pellentesque sit amet dui magna. Sed porta, augue ac bibendum pharetra, nunc ipsum dictum felis, vel tristique justo tortor eu diam. Cras eu risus risus. Fusce iaculis finibus gravida. Sed eu diam pharetra, condimentum orci quis, mollis magna.",
+  },
+  {
+    question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.0",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget odio est. Pellentesque elementum, diam sit amet facilisis gravida, nisi augue rhoncus nibh, quis sodales lorem nisl at ligula. Morbi accumsan iaculis mauris quis semper. Ut sodales justo eu elit pellentesque hendrerit. Aliquam ut ipsum neque. Nulla tincidunt egestas ante quis vestibulum. Suspendisse rhoncus egestas urna, id vestibulum nunc porttitor ac. Pellentesque sit amet dui magna. Sed porta, augue ac bibendum pharetra, nunc ipsum dictum felis, vel tristique justo tortor eu diam. Cras eu risus risus. Fusce iaculis finibus gravida. Sed eu diam pharetra, condimentum orci quis, mollis magna.",
+  },
 ];
 
 export default questions;
