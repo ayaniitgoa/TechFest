072709eef3264b1bfc4a186843dc323586d5d841
Ayan Adhya <ayan.adhya.18003@iitgoa.ac.in>
2020-12-14 11:15:25 +0530

    Trying netlify deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import SiteContainer from "./Containers/SiteContainer/SiteContainer";
 import PageNotFound from "./Containers/404Page/PageNotFound";
 
@@ -8,7 +8,7 @@ import IndividualEventRegistraion from "./Containers/IndividualEventRegister/Ind
 
 function App() {
   return (
-    <Router basename={window.location.pathname || ""}>
+    <Router>
       <div className="App">
         <Switch>
           <Route component={SiteContainer} path="/" exact />
