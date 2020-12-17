fac19049390cf3571f90d91c51fc2e0d9536bbb5
Ayan Adhya <ayan.adhya.18003@iitgoa.ac.in>
2020-12-17 10:23:52 +0530

    Updated links

--- src/Containers/ContactUs/ContactUs.js
+++ src/Containers/ContactUs/ContactUs.js
@@ -12,7 +12,7 @@ import "slick-carousel/slick/slick-theme.css";
 function ContactUs() {
   var settings = {
     // dots: true,
-    infinite: true,
+    infinite: false,
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
--- src/Containers/SocialMedia/SocialMedia.js
+++ src/Containers/SocialMedia/SocialMedia.js
@@ -11,10 +11,18 @@ function SocialMedia() {
           <p>techfest@iitgoa.ac.in</p>
         </div>
         <div className="social-icons">
-          <i className=" fab fa-facebook"></i>
-          <i className="fab fa-instagram"></i>
-          <i className="fab fa-twitter"></i>
-          <i className="fab fa-discord"></i>
+          <a href="https://www.facebook.com/pages/category/Festival/Cepheus-IIT-GOA-103726818293218/">
+            <i className=" fab fa-facebook"></i>
+          </a>
+          <a href="https://instagram.com/cepheus_iitgoa?igshid=yi42u07kqfpa">
+            <i className="fab fa-instagram"></i>
+          </a>
+          <a href="https://twitter.com/cepheus_iitgoa?s=09">
+            <i className="fab fa-twitter"></i>
+          </a>
+          <a href="/">
+            <i className="fab fa-discord"></i>
+          </a>
         </div>
         <div className="copyright-info">
           <p>©2020 - All Rights Reserved - IIT-GOA</p>
