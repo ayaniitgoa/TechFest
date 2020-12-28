56034a030c612f6c818fa7a0adbf15a94a27c471
Ayan Adhya <ayan.adhya.18003@iitgoa.ac.in>
2020-12-28 22:56:10 +0530

    Changed backend url

--- src/variables.js
+++ src/variables.js
@@ -1,7 +1,6 @@
 export const variables = {
-  // backendURL: "https://techfest-cepheus.herokuapp.com",
+  backendURL: "https://techfest-cepheus.herokuapp.com",
 
   // backendURL: "http://localhost:5000",
-  backendURL:
-    "http://cepheus-env.eba-3qi4m4xw.ap-south-1.elasticbeanstalk.com/",
+  // backendURL: "http://cepheus-env.eba-3qi4m4xw.ap-south-1.elasticbeanstalk.com/",
 };
