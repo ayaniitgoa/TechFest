b21982ed4f05b5c85c64be97b332ed7644db5917
Ayan Adhya <ayan.adhya.18003@iitgoa.ac.in>
2021-01-01 19:23:14 +0530

    Added https redirect

--- src/App.js
+++ src/App.js
@@ -9,53 +9,52 @@ import Register from "./Containers/Register/Register";
 import RegistrationSuccess from "./Containers/RegistrationSuccess/RegistrationSuccess";
 import Participants from "./Containers/Participants/Participants";
 import AllUsers from "./Containers/AllUsers/AllUsers";
+import HttpsRedirect from "react-https-redirect";
 // import EventRegistrationSuccess from "./Containers/EventRegistrationSuccess/EventRegistrationSuccess";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route component={SiteContainer} path="/Cepheus" exact />
-          <Route
-            component={IndividualEvent}
-            path="/Cepheus/event/:eventName(fizzbuzz|fiducia|coolyourengine|arduinostrial|uniteforunity|chitchatwithchatbot|cyberexpert|framethecrane|hackoverflow|circuitaldilemma|pandorasboxctf|pythonizeeverything|takecharge|theillusivereality|iceevhybrid)"
-            exact
-          />
-          <Route
-            component={IndividualEventRegistraion}
-            path="/Cepheus/event/:eventName(fizzbuzz|fiducia|coolyourengine|arduinostrial|uniteforunity|chitchatwithchatbot|cyberexpert|framethecrane|hackoverflow|circuitaldilemma|pandorasboxctf|pythonizeeverything|takecharge|theillusivereality|iceevhybrid)/register"
-            exact
-          />
-          <Route
-            component={Register}
-            path="/Cepheus/register"
-            exact
-          />
-          <Route
-            component={RegistrationSuccess}
-            path="/Cepheus/register/success"
-            exact
-          />
-          <Route
-            component={Participants}
-            path="/Cepheus/participants/:eventName(fizzbuzz|fiducia|coolyourengine|arduinostrial|uniteforunity|chitchatwithchatbot|cyberexpert|framethecrane|hackoverflow|circuitaldilemma|pandorasboxctf|pythonizeeverything|takecharge|theillusivereality|iceevhybrid)/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiZmVzdCI6IkNlcGhldXNpaXRnb2F0ZWNoZmVzdDgzMjA2NTM5NTkxMjM0IiwiaWF0IjoxNTE2MjM5MDIyfQ.mSWZCRAJlS7sKxxwgSDMOtZgiHqmpPAADcRZm-BF5X8"
-            exact
-          />
-          <Route
-            component={AllUsers}
-            path="/Cepheus/allusers/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiZmVzdCI6IkNlcGhldXNpaXRnb2F0ZWNoZmVzdDgzMjA2NTM5NTkxMjM0IiwiaWF0IjoxNTE2MjM5MDIyfQ.mSWZCRAJlS7sKxxwgSDMOtZgiHqmpPAADcRZm-BF5X8"
-            exact
-          />
-          {/* <Route
+    <HttpsRedirect>
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route component={SiteContainer} path="/Cepheus" exact />
+            <Route
+              component={IndividualEvent}
+              path="/Cepheus/event/:eventName(fizzbuzz|fiducia|coolyourengine|arduinostrial|uniteforunity|chitchatwithchatbot|cyberexpert|framethecrane|hackoverflow|circuitaldilemma|pandorasboxctf|pythonizeeverything|takecharge|theillusivereality|iceevhybrid)"
+              exact
+            />
+            <Route
+              component={IndividualEventRegistraion}
+              path="/Cepheus/event/:eventName(fizzbuzz|fiducia|coolyourengine|arduinostrial|uniteforunity|chitchatwithchatbot|cyberexpert|framethecrane|hackoverflow|circuitaldilemma|pandorasboxctf|pythonizeeverything|takecharge|theillusivereality|iceevhybrid)/register"
+              exact
+            />
+            <Route component={Register} path="/Cepheus/register" exact />
+            <Route
+              component={RegistrationSuccess}
+              path="/Cepheus/register/success"
+              exact
+            />
+            <Route
+              component={Participants}
+              path="/Cepheus/participants/:eventName(fizzbuzz|fiducia|coolyourengine|arduinostrial|uniteforunity|chitchatwithchatbot|cyberexpert|framethecrane|hackoverflow|circuitaldilemma|pandorasboxctf|pythonizeeverything|takecharge|theillusivereality|iceevhybrid)/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiZmVzdCI6IkNlcGhldXNpaXRnb2F0ZWNoZmVzdDgzMjA2NTM5NTkxMjM0IiwiaWF0IjoxNTE2MjM5MDIyfQ.mSWZCRAJlS7sKxxwgSDMOtZgiHqmpPAADcRZm-BF5X8"
+              exact
+            />
+            <Route
+              component={AllUsers}
+              path="/Cepheus/allusers/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiZmVzdCI6IkNlcGhldXNpaXRnb2F0ZWNoZmVzdDgzMjA2NTM5NTkxMjM0IiwiaWF0IjoxNTE2MjM5MDIyfQ.mSWZCRAJlS7sKxxwgSDMOtZgiHqmpPAADcRZm-BF5X8"
+              exact
+            />
+            {/* <Route
             component={EventRegistrationSuccess}
             path="/register/:eventName/success"
             exact
           /> */}
-          <Route component={PageNotFound} />
-        </Switch>
-      </div>
-    </Router>
+            <Route component={PageNotFound} />
+          </Switch>
+        </div>
+      </Router>
+    </HttpsRedirect>
   );
 }
 
