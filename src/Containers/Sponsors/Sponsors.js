075b13db22f036061a868f9a9000247444ce8951
Ayan Adhya <ayan.adhya.18003@iitgoa.ac.in>
2021-01-06 11:36:30 +0530

    Added sponsors text

--- src/Containers/Sponsors/Sponsors.js
+++ src/Containers/Sponsors/Sponsors.js
@@ -5,21 +5,33 @@ import snpm from "./snpm.png";
 import AI from "./AI.png";
 import AM from "./AM.png";
 import Convergent from "./Convergent.png";
-
 function Sponsors() {
   return (
     <div id="sponsor-section" className="sponsors-section">
       <h2 className="sponsors-title">SPONSORS</h2>
-      {/*<p className="coming-soon-text">Coming Soon..</p>*/}
+      {/* {/<p className="coming-soon-text">Coming Soon..</p>/} */}
       <div className="bubble">
         <div className="bubble-col-1">
-          <img src={AM} className="bubble-img bubble-img-1" alt="" />
-
-          <img src={Convergent} className="bubble-img bubble-img-2" alt="" />
+          <div>
+            <p className="bubble-img-1-title">Title Sponsors</p>
+            <img src={snpm} className="bubble-img bubble-img-1" alt="" />
+          </div>
         </div>
         <div className="bubble-col-2">
-          <img src={AI} className="bubble-img bubble-img-3" alt="" />
-          <img src={snpm} className="bubble-img bubble-img-4" alt="" />
+          <div>
+            <p className="bubble-img-2-title">Platform Partner</p>
+            <img src={AM} className="bubble-img bubble-img-2" alt="" />
+          </div>
+          <div>
+            <p className="bubble-img-3-title">Talk partner</p>
+            <img src={Convergent} className="bubble-img bubble-img-3" alt="" />
+          </div>
+        </div>
+        <div className="bubble-col-3">
+          <div>
+            <p className="bubble-img-4-title">Event Sponsor</p>
+            <img src={AI} className="bubble-img bubble-img-4" alt="" />
+          </div>
         </div>
       </div>
       <img className="poseidon-img" src={poseidon} alt="" />
