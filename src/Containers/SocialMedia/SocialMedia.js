a3a490e139ba3c931af9f521758178320fc81e58
Ayan Adhya <ayan.adhya.18003@iitgoa.ac.in>
2021-01-07 22:21:49 +0530

    Added linkedin

--- src/Containers/SocialMedia/SocialMedia.js
+++ src/Containers/SocialMedia/SocialMedia.js
@@ -33,6 +33,13 @@ function SocialMedia() {
             <i className="fab fa-twitter"></i>
           </a>
           <a
+            href="https://www.linkedin.com/in/cepheus-iit-goa-311230203?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BT1%2FFqBdvTpmsgqSSbdbDEA%3D%3D"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fab fa-linkedin-in"></i>
+          </a>
+          <a
             href="https://discord.gg/bbnt3RFxzH"
             target="_blank"
             rel="noreferrer"
